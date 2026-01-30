02781e636e51050ea1fbcd6b2eb5c7accb54b47d
pablomoreno10 <pablonicolasmc5@gmail.com>
2026-01-30 10:21:51 -0500

    echo command

--- app/main.go
+++ app/main.go
@@ -4,24 +4,36 @@ import (
 	"fmt"
 	"bufio"
 	"os"
+	"strings"
 )
 
 var _ = fmt.Print
 
 func main() {
-	var exit string
+	var exit,echo string
 	exit = "exit"
+	echo = "echo"
 	for{
 		fmt.Print("$ ")
+
 		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
 		if err != nil{
 			fmt.Fprintln(os.Stderr, "Error reading input: " ,err)
 			os.Exit(1)
 		}
-		if command[:len(command)-1] == exit{
+
+		if strings.TrimSpace(command) == exit{
 			os.Exit(0)
 		}
-		fmt.Println(command[:len(command)-1] + ": command not found")
+
+		if  strings.Fields(strings.TrimSpace(command))[0] == echo{
+			slice := strings.Fields(strings.TrimSpace(command))[1:]
+			output := strings.Join(slice, " ")
+			fmt.Println(output)
+		}else{
+			fmt.Println(strings.TrimSpace(command)+ ": command not found")
+		}
 	}
 	
 
