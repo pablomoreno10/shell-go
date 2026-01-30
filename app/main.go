542a444b9cff660eba038f3662f3fe988a1bd13d
pablomoreno10 <pablonicolasmc5@gmail.com>
2026-01-29 21:44:44 -0500

    exit command

--- app/main.go
+++ app/main.go
@@ -9,12 +9,17 @@ import (
 var _ = fmt.Print
 
 func main() {
+	var exit string
+	exit = "exit"
 	for{
 		fmt.Print("$ ")
 		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil{
 			fmt.Fprintln(os.Stderr, "Error reading input: " ,err)
 			os.Exit(1)
+		}
+		if command[:len(command)-1] == exit{
+			os.Exit(0)
 		}
 		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
